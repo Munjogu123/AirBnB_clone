aa7be87bdc6f0a5530c0554f9d932b1ec4c64887
Munjogu123 <fredmunjogu@gmail.com>
2024-03-10 16:24:26 +0300

    adds some tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -21,3 +21,36 @@ class TestFileStorage(unittest.TestCase):
     def test_all(self):
         """ tests the all function """
         self.assertEqual(type(models.storage.all()), dict)
+
+    def test_new(self):
+        """ tests the new function """
+        obj = BaseModel()
+        models.storage.new(obj)
+        self.assertIn(f'BaseModel.{obj.id}', models.storage.all())
+
+    def test_save_reload(self):
+        """ tests the save and reload """
+        obj_1 = BaseModel()
+        obj_2 = BaseModel()
+        models.storage.new(obj_1)
+        models.storage.new(obj_2)
+        models.storage.save()
+
+        f_storage = FileStorage()
+        f_storage.reload()
+
+        self.assertTrue(
+            f_storage.all().get(f'BaseModel.{obj_1.id}') is not None)
+        self.assertTrue(
+            f_storage.all().get(f'BaseModel.{obj_2.id}') is not None)
+
+    def test_save_file(self):
+        """ tests if file is created and contents saved """
+        obj = BaseModel()
+        models.storage.new(obj)
+        models.storage.save()
+        self.assertTrue(os.path.exists(models.storage._FileStorage__file_path))
+
+
+if __name__ == '__main__':
+    unittest.main()
